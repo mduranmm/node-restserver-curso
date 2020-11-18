2205e6c13746e4660da262d845461db2a8f54a6e
Minerva <mduranm@ruv.org.mx>
2020-11-17 18:31:27 -0600

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -13,9 +13,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV) {
-    urlDB = 'mongodb+srv://cafe-mgdb-20:4aCJ3XqcdvBRcwMB@cluster0.kyzx7.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 } else {
-    urlDB = 'mongodb+srv://cafe-mgdb-20:4aCJ3XqcdvBRcwMB@cluster0.kyzx7.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
