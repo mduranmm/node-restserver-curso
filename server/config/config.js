dc32fbaa563f8a3b4600efb26811be92a93bdfb9
Minerva <mduranm@ruv.org.mx>
2021-01-11 17:29:43 -0600

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
+require('dotenv');
 // ================
 // Puerto
 // ================
@@ -11,11 +12,28 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
-
+/*
 if (process.env.NODE_ENV) {
     urlDB = process.env.MONGO_URI;
 } else {
     urlDB = process.env.MONGO_URI;
-}
+}*/
 
-process.env.URLDB = urlDB;
+process.env.URLDB = process.env.MONGO_URI;
+
+//process.env.URLDB = urlDB;
+
+// ================
+// Vencimiento del Token
+// ================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ================
+// SEED de autentificación
+// ================
+process.env.SEED = process.env.SEED || 'this-is-develop-seed';
